96ec9f6a5654dfa0e6be004cedbe56d9307f4783
toemsamat <samattoem212@gmail.com>
2025-06-02 06:54:24 +0700

    first commit

---
+++ portfolio/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Certificate, Profile, Skill, Experience, Project, BlogPost, ContactMessage
+
+admin.site.register(Profile)
+admin.site.register(Skill)
+admin.site.register(Experience)
+admin.site.register(Project)
+admin.site.register(BlogPost)
+admin.site.register(ContactMessage)
+admin.site.register(Certificate)
+
+# Register your models here.
